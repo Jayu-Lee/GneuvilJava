57667ba8b17546e4366bb726a9d4b3d6f0fc09a3
Jayu-Lee <114810429+Jayu-Lee@users.noreply.github.com>
2023-03-15 13:25:42 +0900

    Update VariableExchangeExample.java

--- src/GneuvilJava/VariableExchangeExample.java
+++ src/GneuvilJava/VariableExchangeExample.java
@@ -1,2 +1,12 @@
 package GneuvilJava;public class VariableExchangeExample {
+  public static void main(String[] args) {
+        int x = 3;
+        int y = 5;
+        System.out.println("x:" + x + ", y:" + y);
+
+        int temp = x;
+        x = y;
+        y = temp;
+        System.out.println("x:" + x + ", y:" + y);
+  }
 }
