d52a3b09949eb38a8adf49037ce8c4415fb359a7
Jayu-Lee <114810429+Jayu-Lee@users.noreply.github.com>
2023-03-15 13:28:54 +0900

    Update Hello2.java

--- src/GneuvilJava/Hello2.java
+++ src/GneuvilJava/Hello2.java
@@ -1,2 +1,15 @@
-package GneuvilJava;public class Hello2 {
+package GneuvilJava;
+/**
+ * @author 이수민
+ */
+    /*
+    장제목: 1장 자바 시작하기
+    작성일: 2023.03.13.
+     */
+public class Hello2 {
+    //프로그램 실행 진입점
+    public static void main(String[] arg) {
+        //콘솔에 출력하는 실행문
+        System.out.print("Hello, Java");
+    }
 }
