58ed25bff8acf2e5c20ac8dd58dc1cbca0ea8e2a
Jayu-Lee <160107p@gmail.com>
2023-04-12 12:56:46 +0900

    키보드 입력 데이터를 변수에 저장하기

---
+++ src/GneuvilJava/ScannerExample.java
@@ -0,0 +1,33 @@
+package GneuvilJava;
+
+import java.util.Scanner;
+//Scanner가 java.util 패키지에 있다는 것을 컴파일러에게 알려주는 역할을 한다.
+
+public class ScannerExample {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("x 값 입력: ");
+        String strX = scanner.nextLine();
+        int x = Integer.parseInt(strX);
+
+        System.out.print("y 값 입력: ");
+        String strY = scanner.nextLine();
+        int y = Integer.parseInt(strY);
+
+        int result = x + y;
+        System.out.println("x + y: " + result);
+        System.out.println();
+
+        while(true) {   //중괄호 {} 안을 무한히 반복 실행한다.
+            System.out.print("입력 문자열: ");
+            String data = scanner.nextLine();
+            if(data.equals("q")) {   //입력된 문자열이 q라면 반복 중지
+                break;
+            }
+            System.out.println("출력 문자열: " + data);
+            System.out.println();
+        }
+        System.out.println("종료");
+    }
+}
